58c21e4d2c312520c52c0f3c92c4a84f0564f913
Godday Umah <god21010@byui.edu>
2023-09-23 19:01:54 -0600

    work in progress

---
+++ scripts/w02-task.js
@@ -0,0 +1,48 @@
+
+let fullname = 'Umah Godday';
+
+//const currentYear = currentDate.getFullYear();
+const currentYear = new Date().getFullYear();   
+const directory = 'images/';
+const fileName = 'myself.jpg';
+const profilePicture = directory + fileName;
+console.log('profile picture:', profilePicture);
+
+const nameElement = document.getElementById('name');
+nameElement.textContent = fullname
+//const foodElement = document.getElementById('food');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+
+
+//const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('profilePicture');
+yearElement.textContent = currentYear;
+
+
+
+nameElement.innerHTML = `<strong>${fullname}</strong>`; 
+yearElement.textContent = currentYear.toString();
+
+
+
+
+//const favoriteFoods = ['bread','Fried Rice','Pizza','Ramen','Halal Burger'];
+//const foodText = favoriteFoods.join(', ');
+
+const favoriteFoods = ['Bread','Fried Rice','Pizza','Ramen','Halal Burger'];
+foodElement.textContent = favoriteFoods
+
+
+imageElement.setAttribute('src', console());
+imageElement.setAttribute('alt', `profile image of ${fullname}`);
+//foodElement.textContent = foodText;
+
+const delicious= ['Mac n Cheese'];
+delicious.push(favoriteFoods);
+foodElement.textContent = favoriteFoods
+foodElement.innerHTML+= `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += favoriteFoods;
+favoriteFoods.pop();
+foodElement.innerHTML += favoriteFoods;
--- sketch/work.js
+++ sketch/work.js
@@ -1,5 +1,15 @@
-var firstname = "Umah Godday";
-let firstname = "Umah Godday";
-firstname = "new value";
-const firstname = "Umah Godday"
- 
+const individual = {
+    name : "david",
+    age : 28,
+    height : 10.5,
+    address : "virginia united state"
+};
+delete individual.age
+
+//print(individual)
+
+const A1 = ["jan","feb","mar"];
+const A2 = ["apr","may","jun"];
+const A3 = ["jul","aug","sept"];
+
+const year= [...A1,...A2,...A3];
