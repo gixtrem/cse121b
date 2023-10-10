9923a9f8a91db85472981c44afaa41a90874de82
Godday Umah <god21010@byui.edu>
2023-10-09 21:58:40 -0600

    half

---
+++ scripts/w04-task.js
@@ -0,0 +1,42 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+const myProfile = {};
+
+myProfile.name = "Umah Godday";
+myProfile.photo = "myself.jpg";
+myProfile.favoriteFoods = ["Rice", "Tikka Masala", "Prioshki", "shrimp", "Banana Cream Pie"];
+myProfile.hobbies = ["Volleyball", "Soccer", "Basket ball"];
+// myProfile.placeLived = ["Nigeria", "Ghana", "Dubai", "USA"];
+myProfile.placeLived = [];
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placeLived.push({place: "Nigeria", length: "12 years"});
+myProfile.placeLived.push({place: "Ghana", length: "3 years"});
+myProfile.placeLived.push({place: "Dubai", length: "4 years"});
+myProfile.placeLived.push({place: "USA", length: "2 years"});
+
+
+
+
+
+/* Name */
+
+
+
+/* DOM Manipulation - Output */
+
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
