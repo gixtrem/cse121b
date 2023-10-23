1d7159d18006c8c9502842cdc47292ea2a126227
Godday Umah <god21010@byui.edu>
2023-10-23 14:53:03 -0600

    fuuuu

--- script/final.js
+++ script/final.js
@@ -1,12 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",() =>{
     const quoteElement = document.getElementById("quote");
     const authorElement = document.getElementById("author");
     const newQuoteButton = document.getElementById("new-quote-button");
+    const loader = document.getElementById("loader");
   
     const quotesApiUrl = "https://api.quotable.io/random"; 
   
+    
+    const changeBackgroundColor = () => {
+      const colors = ["#766F74", "#291A24", "#321629", "#A33081", "#571845"];
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      document.body.style.backgroundColor = colors[randomIndex];
+    };
+  
+    
+    const showLoading = () => {
+      loader.classList.remove("hidden");
+    };
+  
+  
+    const hideLoading = () => {
+      loader.classList.add("hidden");
+    };
+  
     // Function to fetch and display a new quote
     const fetchNewQuote = () => {
+      showLoading();
       fetch(quotesApiUrl)
         .then((response) => {
           if (response.ok) {
@@ -16,18 +35,21 @@ document.addEventListener("DOMContentLoaded", () => {
           }
         })
         .then((data) => {
-          quoteElement.textContent = data.content; 
-          authorElement.textContent = `- ${data.author}`; 
+          quoteElement.textContent = data.content;
+          authorElement.textContent = `- ${data.author}`;
+          changeBackgroundColor();
+          hideLoading();
         })
         .catch((error) => {
           console.error(`Error: ${error}`);
+          hideLoading();
         });
     };
   
     // Fetch the initial quote
     fetchNewQuote();
   
-    // Set up the button click event
+    
     newQuoteButton.addEventListener("click", fetchNewQuote);
   });
   
