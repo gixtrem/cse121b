c8ef5abf9df54e550d33363d14ece413c4c51f5a
Godday Umah <god21010@byui.edu>
2023-10-25 20:39:34 -0600

    WEEK 4 DONE

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,36 +1,31 @@
 /* LESSON 3 - Programming Tasks */
 
-/* Profile Object  */
+/* Profile Object */
 const myProfile = {};
 
 myProfile.name = "Umah Godday";
-myProfile.photo = "images/myself.jpg";
-Image.src = "myself.jpg"
-photo.alt = "An updated image"
+myProfile.photo = "images/myself.jpg"; // Assuming the path is correct
 myProfile.favoriteFoods = ["Rice", "Tikka Masala", "Prioshki", "shrimp", "Banana Cream Pie"];
-myProfile.hobbies = ["Volleyball", "Soccer", "Basket ball"];
-// myProfile.placeLived = ["Nigeria", "Ghana", "Dubai", "USA"];
+myProfile.hobbies = ["Volleyball", "Soccer", "Basketball"]; // Fixed typo in "Basket ball"
 myProfile.placesLived = [];
 
-/* Populate Profile Object with placesLive objects */
-myProfile.placesLived.push(
-    {
-        place:'Rexburg, USA',
-        length : '2 year'
-    }
-);
-myProfile.placesLived.push({place: "Lagos, Nigeria", length: "12 year"});
-myProfile.placesLived.push({place: "Accra, Ghana", length: "3 year"});
-myProfile.placesLived.push({place: "Emirate, Dubai", length: "4 year"});
-
+myProfile.placesLived.push({place: "San Francisco", length: "3 years"});
+myProfile.placesLived.push({place: "Rexburg, USA", length: "2 years"}); // Fixed typo in "year"
+myProfile.placesLived.push({place: "Lagos, Nigeria", length: "12 years"}); // Fixed typo in "year"
+myProfile.placesLived.push({place: "Accra, Ghana", length: "3 years"}); // Fixed typo in "year"
+myProfile.placesLived.push({place: "Emirate, Dubai", length: "4 years"}); // Fixed typo in "year"
 
 /* DOM Manipulation - Output */
 
-/* Photo with attributes */
+// Name
 document.getElementById("name").textContent = myProfile.name;
-document.getElementById("photo").textContent = myProfile.photo;
-document.getElementById("photo").alt = myProfile.name;
 
+// Photo with attributes
+const photoElement = document.getElementById("photo");
+photoElement.src = myProfile.photo;
+photoElement.alt = myProfile.name;
+
+// Favorite Foods List
 const favoriteFoodsList = document.getElementById("favorite-foods");
 myProfile.favoriteFoods.forEach(food => {
     const li = document.createElement("li");
@@ -38,26 +33,15 @@ myProfile.favoriteFoods.forEach(food => {
     favoriteFoodsList.appendChild(li);
 });
 
-
+// Hobbies List
 const hobbiesList = document.getElementById("hobbies");
 myProfile.hobbies.forEach(hobby => {
     const li = document.createElement("li");
     li.textContent = hobby;
     hobbiesList.appendChild(li);
-})
+});
 
-// const placesLivedDL =document.getElementById("places-lived");
-// myProfile.placesLived.forEach(place => {
-//     const dt = document.createElement("dt");
-//     dt.textContent = place.place;
-
-//     const dd = document.createElement("dd");
-//     dd.textContent = place.length;
-
-//     placesLivedDL.appendChild(dt);
-//     placesLivedDL.appendChild(dd);
-// })
-
+// Places Lived DataList
 myProfile.placesLived.forEach(function (place) {
     let dt = document.createElement("dt");
     dt.textContent = place.place;
@@ -68,23 +52,4 @@ myProfile.placesLived.forEach(function (place) {
     let dl = document.getElementById("places-lived");
     dl.appendChild(dt);
     dl.appendChild(dd);
-
-})
-
-/* Name */
-
-
-
-
-
-
-
-/* Favorite Foods List*/
-
-
-/* Hobbies List */
-
-
-/* Places Lived DataList */
-
-
+});
